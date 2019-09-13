e8e72847442dfa5392e5270e87964f8477b9adfc
vdilanyan <vdilanyan@gmail.com>
2019-09-13 21:32:55 +0200

    replaced single-quotes with double-quotes

--- functions/acf-fields.php
+++ functions/acf-fields.php
@@ -12,18 +12,18 @@ if (function_exists("acf_add_options_page")) {
 	]);
 }
 
-$theme_settings = new FieldsBuilder('settings');
+$theme_settings = new FieldsBuilder("settings");
 $theme_settings
-	->addRepeater('social_links')
-		->addImage('icon', [
-			'return_format' => 'url',
+	->addRepeater("social_links")
+		->addImage("icon", [
+			"return_format" => "url",
 		])
 		->setWidth(50)
-		->addUrl('link')
+		->addUrl("link")
 		->setWidth(50)
 	->endRepeater()
-	->setLocation('options_page', '==', 'theme-general-settings');
+	->setLocation("options_page", "==", "theme-general-settings");
 
-add_action('acf/init', function() use ($theme_settings) {
+add_action("acf/init", function() use ($theme_settings) {
 	acf_add_local_field_group($theme_settings->build());
 });
