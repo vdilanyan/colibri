f7d6ab7b8a40d245de598024e0b343c6859b8c1e
vdilanyan <vdilanyan@gmail.com>
2019-09-24 11:19:31 +0200

    changed the map color and address. fixed the product hover state on single product page.

--- contacts.php
+++ contacts.php
@@ -21,104 +21,18 @@ Timber::render('templates/contacts.twig', $context);
 <script type="text/javascript">
 (function($) {
 	function new_map($el) {
-		var styles = [
-			{elementType: 'geometry', stylers: [{color: '#242f3e'}]},
-			{elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
-			{elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
-			{
-				featureType: 'administrative.locality',
-				elementType: 'labels.text.fill',
-				stylers: [{color: '#949494'}]
-			},
-			{
-				featureType: 'poi',
-				elementType: 'labels.text.fill',
-				stylers: [{color: '#949494'}]
-			},
-			{
-				featureType: 'poi.park',
-				elementType: 'geometry',
-				stylers: [{color: '#343332'}]
-			},
-			{
-				featureType: 'poi.park',
-				elementType: 'labels.text.fill',
-				stylers: [{color: '#949494'}]
-			},
-			{
-				featureType: 'road',
-				elementType: 'geometry',
-				stylers: [{color: '#343332'}]
-			},
-			{
-				featureType: 'road',
-				elementType: 'geometry.stroke',
-				stylers: [{color: '#343332'}]
-			},
-			{
-				featureType: 'road',
-				elementType: 'labels.text.fill',
-				stylers: [{color: '#949494'}]
-			},
-			{
-				featureType: 'road.highway',
-				elementType: 'geometry',
-				stylers: [{color: '#343332'}]
-			},
-			{
-				featureType: 'road.highway',
-				elementType: 'geometry.stroke',
-				stylers: [{color: '#343332'}]
-			},
-			{
-				featureType: 'road.highway',
-				elementType: 'labels.text.fill',
-				stylers: [{color: '#949494'}]
-			},
-			{
-				featureType: 'transit',
-				elementType: 'geometry',
-				stylers: [{color: '#343332'}]
-			},
-			{
-				featureType: 'transit.station',
-				elementType: 'labels.text.fill',
-				stylers: [{color: '#949494'}]
-			},
-			{
-				featureType: 'water',
-				elementType: 'geometry',
-				stylers: [{color: '#191a1a'}]
-			},
-			{
-				featureType: 'water',
-				elementType: 'labels.text.fill',
-				stylers: [{color: '#949494'}]
-			},
-			{
-				featureType: 'water',
-				elementType: 'labels.text.stroke',
-				stylers: [{color: '#949494'}]
-			}
-		];
-
-		var styledMap = new google.maps.StyledMapType(styles,{name: "Styled Map"});
-
 		var $markers = $el.find('.marker');
 		
 		var args = {
 			zoom: 16,
 			center: new google.maps.LatLng(0, 0),
 			mapTypeControlOptions: {
-				mapTypeIds: [google.maps.MapTypeId.ROADMAP, 'map_style']
+				mapTypeIds: [google.maps.MapTypeId.ROADMAP]
 			},
 			disableDefaultUI: true,
 		};
 
 		var map = new google.maps.Map($el[0], args);
-
-		map.mapTypes.set('map_style', styledMap);
-		map.setMapTypeId('map_style');
 
 		map.markers = [];
 
@@ -155,14 +69,14 @@ Timber::render('templates/contacts.twig', $context);
 	function center_map(map) {
 		var bounds = new google.maps.LatLngBounds();
 
-		$.each( map.markers, function(i, marker) {
+		$.each(map.markers, function(i, marker) {
 			var latlng = new google.maps.LatLng(marker.position.lat(), marker.position.lng());
 			bounds.extend( latlng );
 		});
 
-		if(map.markers.length == 1) {
+		if (map.markers.length == 1) {
 			map.setCenter(bounds.getCenter());
-			map.setZoom(16);
+			map.setZoom(18);
 		} else {
 			map.fitBounds(bounds);
 		}
